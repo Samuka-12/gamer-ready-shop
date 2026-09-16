20e8ae114fe0097c372596fcb9b52bb9211c8811
samuel c luz <samuelcab444@gmail.com>
2026-09-16 17:29:03 -0300

    Adiciona versao standalone em HTML puro e servidor local

---
+++ standalone/server.cjs
@@ -0,0 +1,43 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const MIME_TYPES = {
+  '.html': 'text/html; charset=UTF-8',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.webp': 'image/webp',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+};
+
+const server = http.createServer((req, res) => {
+  let reqPath = req.url.split('?')[0];
+  if (reqPath === '/' || reqPath === '') reqPath = '/index.html';
+  
+  const filePath = path.join(__dirname, reqPath);
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain; charset=UTF-8' });
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500);
+        res.end('Server Error: ' + err.code);
+      }
+    } else {
+      const ext = path.extname(filePath).toLowerCase();
+      res.writeHead(200, { 'Content-Type': MIME_TYPES[ext] || 'application/octet-stream' });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Servidor rodando em http://localhost:' + PORT);
+});
